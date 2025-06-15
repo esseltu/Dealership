3e03cbe6f0e140dc75ca7dd168995082485137be
unknown <esselturkson1@gmail.com>
2025-06-15 16:25:05 +0000

    removed contact page and fixed mobile css

--- script.js
+++ script.js
@@ -351,7 +351,7 @@ function renderVehicles(vehicles) {
         
         <div class="card-actions">
           <button class="btn btn-primary flex-1" onclick="window.location.href='details.html?id=${vehicle.id}'">View Details</button>
-          <button class="btn btn-outline icon-only" title="Contact Dealer" onclick="window.location.href='contact.html'">
+          <button class="btn btn-outline icon-only" title="Contact Dealer" onclick="window.location.href='#'">
             <svg width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
               <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
             </svg>
